3ce46be718aa5277e659d2fc048fa329991ab5a5
Jung Jae gook <Jung Jae gook@DESKTOP-6IKHTBU>
2022-02-24 01:21:53 +0900

    h2 디비 추가

---
+++ src/main/java/com/company/mypage/web/domain/sample/entity/Sample.java
@@ -0,0 +1,18 @@
+package com.company.mypage.web.domain.sample.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Sample {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+    
+    //getter, setter 생략
+}
