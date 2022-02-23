016b65effd554b8c054baec0f8c0ee8170455316
Jung Jae gook <Jung Jae gook@DESKTOP-6IKHTBU>
2022-02-23 22:38:29 +0900

    프로젝트 초기 셋팅

---
+++ src/main/java/com/company/mypage/web/domain/sample/SampleController.java
@@ -0,0 +1,8 @@
+package com.company.mypage.web.domain.sample;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class SampleController {
+
+}
---
+++ src/main/java/com/company/mypage/web/domain/sample/service/SampleService.java
@@ -0,0 +1,8 @@
+package com.company.mypage.web.domain.sample.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SampleService {
+
+}
